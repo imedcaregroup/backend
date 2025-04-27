48b53f4bd77c7851ec99ccac5316991d241616c3
i80586 <rasim.ashurov@gmail.com>
2025-04-28 00:49:26 +0400

    feat(order): add ability to include employee when create order

--- src/validations/orderValidation.ts
+++ src/validations/orderValidation.ts
@@ -1,10 +1,25 @@
 import { check } from "express-validator";
+import prisma from "../config/db";
 
 export const createOrderValidation = [
   // check("serviceId").notEmpty().withMessage("Please provide service"),
   // check("categoryId").notEmpty().withMessage("Please provide category"),
   // check("subCategoryId").notEmpty().withMessage("Please provide subCategoryId"),
   check("medicalId").notEmpty().withMessage("Please provide medical"),
+  check('employeeId')
+      .optional({ nullable: true, checkFalsy: true })
+      .isInt()
+      .withMessage('Please provide employeeId')
+      .bail()
+      .custom(async (value) => {
+        const employee = await prisma.employee.findUnique({
+          where: { id: value },
+        });
+
+        if (!employee) {
+          throw new Error('Employee with given ID does not exist');
+        }
+      }),
   check("address").notEmpty().withMessage("Please provide address"),
   check("lat").optional({ nullable: true, checkFalsy: true }).isFloat(),
   check("lng").optional({ nullable: true, checkFalsy: true }).isFloat(),
