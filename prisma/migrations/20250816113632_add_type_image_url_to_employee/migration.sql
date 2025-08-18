23fe44551fe47984f6dd24626257f87f16632b7b
i80586 <rasim.ashurov@gmail.com>
2025-08-18 23:21:06 +0400

    fix: remove dropping table specialization from migrations

--- prisma/migrations/20250816113632_add_type_image_url_to_employee/migration.sql
+++ prisma/migrations/20250816113632_add_type_image_url_to_employee/migration.sql
@@ -1,15 +1,6 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Specialization` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "EmployeeType" AS ENUM ('DOCTOR', 'NURSE');
 
 -- AlterTable
 ALTER TABLE "Employee" ADD COLUMN     "imageUrl" TEXT,
 ADD COLUMN     "type" "EmployeeType" NOT NULL DEFAULT 'NURSE';
-
--- DropTable
-DROP TABLE "Specialization";
