b639f35ae779952e187a999023db64b7155f27ec
Timur Karimov <timur.karimov89@gmail.com>
2025-08-25 13:55:57 +0400

    revert

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -80,16 +80,7 @@ export class EmployeeService {
 
   public async getDoctor(id: number): Promise<any> {
       const employee = await prisma.employee.findUnique({
-          select: {
-              id: true,
-              name: true,
-              surName: true,
-              position: true,
-              experienceYears: true,
-              about_az: true,
-              about_ru: true,
-              about_en: true,
-              imageUrl: true,
+          include: {
               medical: { select: { id: true, name: true, iconUrl: true } },
               employeeCategories: {
                   include: {
