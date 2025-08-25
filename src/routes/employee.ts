10e701c8d2a5d71110350aad83c3a2c341a4008a
t-moor <timur.karimov89@gmail.com>
2025-08-25 12:03:56 +0400

    Update employee.ts

--- src/routes/employee.ts
+++ src/routes/employee.ts
@@ -6,6 +6,6 @@ const employeeController = EmployeeController();
 const router = Router();
 
 router.route("/doctors").get(employeeController.getDoctors);
-router.route("/doctor/:id").get(employeeController.getDoctor);
+router.route("/doctors/:id").get(employeeController.getDoctor);
 
 export default router;
